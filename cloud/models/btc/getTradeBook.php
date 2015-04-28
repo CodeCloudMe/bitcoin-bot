fb160c109be803f13ea1ae4234d2eee4dce06478
chinara <chinara.isabaeva@gmail.com>
2015-04-28 15:03:32 +0100

    fix problem with trade book from okcoin

--- cloud/models/btc/getTradeBook.php
+++ cloud/models/btc/getTradeBook.php
@@ -7,23 +7,32 @@
 	ini_set('display_errors',1);  error_reporting(E_ALL); 
 
 
-	$resp = getTradeBook();
+	$resp = getBook();
 
+
+	echo(json_encode($resp));
 
 	//prop
 	
-	function getTradeBook(){
+	function getBook(){
 		$info = file_get_contents("https://www.okcoin.com/api/v1/trades.do?symbol=btc_usd");
 		$tInfo = json_decode($info, true);
 		for($i=0; $i < count($tInfo); $i++){
-            $amount = $tInfo[$i]['amount'];
+        try{
             $price = $tInfo[$i]['price'];
+            $amount = $tInfo[$i]['amount'];
             $type = $tInfo[$i]['type'];
-
+            $tid = strval($tInfo[$i]['tid']);
+        }
         
-        dbQuery("INSERT INTO okcoinTradeBook(amount, price, type) VALUES ($amount, $price, $type)");
-        echo(dbQuery("INSERT INTO okcoinTradeBook(amount, price, type) VALUES ($amount, $price, $type)"));
+        catch(exception $e){
+
+        }
+        
+        dbQuery("INSERT INTO okcoinTradeBook(amount, price, type, tid) VALUES ($amount, $price, $type, $tid)");
+        echo(dbQuery("INSERT INTO okcoinTradeBook(amount, price, type, tid) VALUES ($amount, $price, $type, $tid)"));
 		}
+			
 		return $tInfo;
 	}
 
