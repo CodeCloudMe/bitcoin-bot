a32b57542e4e0213f539fff1ef6f89af67517f7e
chinara <chinara.isabaeva@gmail.com>
2015-04-28 11:15:48 +0100

    added trade book for okcoin

--- cloud/models/btc/getTradeBook.php
+++ cloud/models/btc/getTradeBook.php
@@ -10,26 +10,20 @@
 	$resp = getTradeBook();
 
 
-	echo(json_encode($resp));
-
 	//prop
 	
 	function getTradeBook(){
 		$info = file_get_contents("https://www.okcoin.com/api/v1/trades.do?symbol=btc_usd");
 		$tInfo = json_decode($info, true);
 		for($i=0; $i < count($tInfo); $i++){
-        try{
+
             $amount = $tInfo[$i]['amount'];
             $price = $tInfo[$i]['price'];
             $type = $tInfo[$i]['type'];
             $tid = $tInfo[$i]['tid'];
-        }
-        
-        catch(exception $e){
 
-        }
         
-        dbQuery("INSERT INTO okcoinTradeBook(amount, price, type, transactionId) VALUES ($amount, $price, $type, $tid)");
+        dbQuery("INSERT INTO okcoinTradeBook(price, amount, type, transactionId) VALUES ($amount, $price, $type, $tid)");
         echo(dbQuery("INSERT INTO okcoinTradeBook(amount, price, type, transactionId) VALUES ($amount, $price, $type, $tid)"));
 		}
 		return $tInfo;
