ec919074675e7aa36d7f86eae247c024302e957e
chinara <chinara.isabaeva@gmail.com>
2015-04-28 22:55:21 +0100

    fix code

--- cloud/models/btc/getTradeBook.php
+++ cloud/models/btc/getTradeBook.php
@@ -29,7 +29,7 @@
 
         }
         
-        dbQuery("INSERT INTO okcoinTradeBook(amount, price, type, tid) VALUES ($amount, $price, $type, $tid)");
+        dbQuery("INSERT INTO okcoinTradeBook(amount, price, type, tid) VALUES ($amount, $price, '$type', $tid)");
         echo("INSERT INTO okcoinTradeBook(amount, price, type, tid) VALUES ($amount, $price, $type, $tid)");
 		}
 			
